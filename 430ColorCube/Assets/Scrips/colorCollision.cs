ec53c8e834aadbfc455b656df9524c6af0871d15
bborrelli <bborrelli@mail.csuchico.edu>
2014-12-03 08:35:36 -0800

    Updating project 2nd

--- 430ColorCube/Assets/Scrips/colorCollision.cs
+++ 430ColorCube/Assets/Scrips/colorCollision.cs
@@ -3,42 +3,114 @@ using System.Collections;
 
 public class colorCollision : MonoBehaviour {
 	public string curColor = "clear";
+	public string secColor = "clear";
+	Mesh purple;
+	Mesh green;
+	Mesh orange;
+	public Material purpleMat;
+	public Material greenMat;
+	public Material orangeMat;
+	public GameObject purp;	
+	public GameObject oran;
+	public GameObject gren;
+	Mesh initialMesh;
+	Mesh changeMesh;
+
 
 	public Color lerpedColor = Color.white;
 
 	// Use this for initialization
 	void Start () {
-
-	
+		initialMesh = GetComponent<MeshFilter>().mesh;
+		purple = purp.GetComponent<MeshFilter>().mesh;
+		orange = oran.GetComponent<MeshFilter>().mesh;
+		green = gren.GetComponent<MeshFilter>().mesh;
 	}
 	
 	// Update is called once per frame
 	void OnTriggerEnter(Collider collision) {
 		print("collide");
+		changeMesh = collision.GetComponent<MeshFilter>().mesh;
 		lerpedColor = gameObject.renderer.material.color;
 		if(collision.gameObject.tag == "Blue"){
-			while(lerpedColor != Color.blue){
-				gameObject.renderer.material.color = Color.Lerp(lerpedColor, Color.blue, Time.time);
-				lerpedColor = gameObject.renderer.material.color;
+			if(lerpedColor == Color.red){
+				gameObject.renderer.material = purpleMat;
+				GetComponent<MeshFilter>().mesh = purple;
+				curColor = "blue";
+				secColor = "red";
 			}
-			curColor = "blue";
-			Destroy(collision.gameObject);
+			else if(lerpedColor == Color.yellow){
+				gameObject.renderer.material = greenMat;
+				GetComponent<MeshFilter>().mesh = green;
+				curColor = "blue";
+				secColor = "yellow";
+			}
+			else{
+				gameObject.renderer.material.color = Color.Lerp(lerpedColor, Color.blue, Time.time);
+				GetComponent<MeshFilter>().mesh = changeMesh;
+				lerpedColor = gameObject.renderer.material.color;
+				curColor = "blue";
+				secColor = "clear";
+			}
+
+
+			collision.gameObject.GetComponent<spawner>().onPickup();
 		}
 		if(collision.gameObject.tag == "Red"){
-			while(lerpedColor != Color.red){
-				gameObject.renderer.material.color = Color.Lerp(lerpedColor, Color.red, Time.time);
-				lerpedColor = gameObject.renderer.material.color;
+			if(lerpedColor == Color.blue){
+				gameObject.renderer.material = purpleMat;
+				GetComponent<MeshFilter>().mesh = purple;
+				curColor = "red";
+				secColor = "blue";
 			}
-			curColor = "red";
-			Destroy(collision.gameObject);
+			else if(lerpedColor == Color.yellow){
+				gameObject.renderer.material = orangeMat;
+				GetComponent<MeshFilter>().mesh = orange;
+				curColor = "red";
+				secColor = "yellow";
+			}
+			else{
+				gameObject.renderer.material.color = Color.Lerp(lerpedColor, Color.red, Time.time);
+				GetComponent<MeshFilter>().mesh = changeMesh;
+				lerpedColor = gameObject.renderer.material.color;
+				curColor = "red";
+				secColor = "clear";
+			}
+
+			collision.gameObject.GetComponent<spawner>().onPickup();
+			//Destroy(collision.gameObject);
 		}
 		if(collision.gameObject.tag == "Yellow"){
-			while(lerpedColor != Color.yellow){
+			if(lerpedColor == Color.blue){
+				gameObject.renderer.material = greenMat;
+				GetComponent<MeshFilter>().mesh = green;
+				curColor = "yellow";
+				secColor = "blue";
+			}
+			else if(lerpedColor == Color.red){
+				gameObject.renderer.material = orangeMat;
+				GetComponent<MeshFilter>().mesh = orange;
+				curColor = "yellow";
+				secColor = "red";
+			}
+			else{
 				gameObject.renderer.material.color = Color.Lerp(lerpedColor, Color.yellow, Time.time);
+				GetComponent<MeshFilter>().mesh = changeMesh;
+				lerpedColor = gameObject.renderer.material.color;
+				curColor = "yellow";
+				secColor = "clear";
+			}
+
+			collision.gameObject.GetComponent<spawner>().onPickup();
+		}
+		if(collision.gameObject.tag == "Black"){
+			while(lerpedColor != Color.white){
+				gameObject.renderer.material.color = Color.Lerp(lerpedColor, Color.white, Time.time);
+				GetComponent<MeshFilter>().mesh = initialMesh;
 				lerpedColor = gameObject.renderer.material.color;
 			}
-			curColor = "yellow";
-			Destroy(collision.gameObject);
+			curColor = "clear";
+			secColor = "clear";
 		}
 	}
 }
--- 430ColorCube/Assets/Scrips/movement.cs
+++ 430ColorCube/Assets/Scrips/movement.cs
@@ -3,41 +3,79 @@ using System.Collections;
 
 public class movement : MonoBehaviour {
 	public GameObject player;
-
+	int chargeTime = 0;
+	public bool smash = false;
+	bool charge = true;
 	int jumpSpeed = 15;
 	int playerSpeed = 10;
 	public float gravity = 20.0F;
 	private Vector3 moveDirection = Vector3.zero;
+	public Camera playerCam;
+	public Camera mainCam;
+
+	void Start(){
+		mainCam.camera.enabled = false;
+		playerCam.camera.enabled = true;
+	}
+
 	void Update() {
 		CharacterController controller = GetComponent<CharacterController>();
 		float MoveHorizontal = Input.GetAxis("Horizontal") * playerSpeed * Time.deltaTime;
 		float MoveVertical = Input.GetAxis("Vertical") * playerSpeed * Time.deltaTime;
-		
 		// Move the Player
 	//	transform.Translate(Vector3.forward * MoveVertical);
 		transform.Rotate(0, playerSpeed * MoveHorizontal, 0);
 		if (controller.isGrounded) {
 			moveDirection = new Vector3(0, 0, Input.GetAxis("Vertical"));
 			moveDirection = transform.TransformDirection(moveDirection * playerSpeed);
-			if (Input.GetButton("Jump")){
-				if(player.GetComponent<colorCollision>().curColor == "yellow")
-					moveDirection.y = jumpSpeed*2;
+			if (Input.GetButton("Charge") && player.GetComponent<colorCollision>().curColor == "red" || player.GetComponent<colorCollision> ().curColor == "red") {
+				if(charge == true){
+					smash = true;
+					float curSpeed = playerSpeed * Input.GetAxis("Vertical");
+					Vector3 forward = transform.TransformDirection(Vector3.forward);
+					controller.SimpleMove(forward * curSpeed* 10);
+					chargeTime++;
+					if(chargeTime == 30){
+						charge = false;
+						smash = false;
+					}
+				}
+			}
+			else{
+				smash = false;
+			}
+			if(Input.GetButtonUp("Charge")){
+			StartCoroutine("Recharge");
+			}
+			if (Input.GetButtonDown("Jump")){
+				if(player.GetComponent<colorCollision>().curColor == "yellow" || player.GetComponent<colorCollision> ().curColor == "yellow")
+					moveDirection.y = jumpSpeed*1.5f;
 				else
 					moveDirection.y = jumpSpeed;
 			}
 		}
-		if (player.GetComponent<colorCollision>().curColor == "blue" && controller.isGrounded == false){
-			if(Input.GetButtonDown("Jump")){
-				gravity = .05f;
-				moveDirection.y = jumpSpeed;
-
+		if (player.GetComponent<colorCollision>().curColor == "blue" && controller.isGrounded == false || player.GetComponent<colorCollision> ().curColor == "blue"){
+			if(Input.GetButton("Jump") && moveDirection.y < 1f){
+				gravity = 30f;
 				moveDirection = new Vector3(0, 0, Input.GetAxis("Vertical"));
-				moveDirection = transform.TransformDirection(moveDirection * playerSpeed*15);
-
+				moveDirection = transform.TransformDirection(moveDirection * playerSpeed);
 			}
+
 		}
-		gravity = 20.0f;
-		moveDirection.y -= gravity * Time.deltaTime;
+		if (player.GetComponent<colorCollision> ().curColor == "blue" || player.GetComponent<colorCollision> ().secColor == "blue") {
+			moveDirection.y -= gravity * Time.deltaTime;
+		}
+		else {
+			moveDirection.y -= gravity * Time.deltaTime;
+
+		}
 		controller.Move(moveDirection * Time.deltaTime);
+		gravity = 20f;
 	}
+	IEnumerator Recharge(){
+		yield return new WaitForSeconds (3f);
+		charge = true;
+		chargeTime = 0;
+	}
+
 }
